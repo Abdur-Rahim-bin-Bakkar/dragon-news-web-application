e8a122e202938237d2a320d7c19b38b3f0022af2
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-25 07:01:11 +0600

    install daisy UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
-  return(
+  return (
     <div>
       <h1 className="text-red-500">this is heading of main page</h1>
     </div>
