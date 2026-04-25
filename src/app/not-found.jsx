d7653a616c6a9349796c73990158b2f81ef5e69c
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-25 08:47:17 +0600

    install react fast marquee

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import img from '@/assets/error-404.png'
 import Image from 'next/image';
+import Link from 'next/link';
 
 const NotFountPage = () => {
     return (
         <div className='h-screen flex justify-center items-center flex-col space-y-3'>
             <h1 className='font-bold text-4xl text-purple-500 mb-4'>Not Fount your Item</h1>
             <Image src={img} alt='not found image' width={200} height={200} className='max-w-120 mx-auto'></Image>
+            <Link className='btn btn-success' href={'/'}>Go Back Home</Link>
         </div>
     );
 };
--- src/components/Top/Header.jsx
+++ src/components/Top/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from '@/assets/logo.png'
+import Image from 'next/image';
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
+        <div className='py-10 text-center'>
+            <Image src={logo} alt='header logo' width={300} height={200} className='max-w-80 mx-auto'/>
+            <p className='mt-5 mb-3 text-[#706F6F]'>Journalism Without Fear or Favour</p>
+            <p className='text-[#706F6F]'>{format(new Date(),'EEEE, MMM dd, yyyy')}</p>
         </div>
     );
 };
