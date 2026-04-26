0fa6b4f049b6c82da96e5a159a1d0b63a89ce7f8
webdesignrahim4061 <webdesignrahim4061@gmail.com>
2026-04-26 07:11:27 +0600

    create register page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -8,10 +8,10 @@ const LoginPage = () => {
                 <h1 className='text-center font-bold text-2xl border-b border-stone-300 pb-5'>Login your Account</h1>
 
                 <form action="" className='mt-5 space-y-5'>
-                    <label  className='font-bold ' >Email Address</label><br />
-                    <input type="email" className='input bg-stone-100 mt-3' placeholder='Enter Your Email' name="" id="" /><br />
+                    <label className='font-bold ' >Email Address</label><br />
+                    <input type="email" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Email' name="" id="" /><br />
                     <label className='font-bold ' >Email Address</label>
-                    <input type="email" className='input bg-stone-100 mt-3' placeholder='Enter Your Email' name="" id="" />
+                    <input type="email" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Email' name="" id="" />
                     <button type='submit' className='btn bg-[#403F3F] w-full text-white font-bold'>Login</button>
                 </form>
                 <p className='text-[#403F3F] text-center mt-3'>Dont’t Have An Account ? <Link href={'/register'} className=' text-[#F75B5F] font-bold cursor-pointer'>Register</Link></p>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 const RegisterPage = () => {
     return (
-        <div>
-            <h1>this is register page</h1>
+              <div className='bg-stone-100 h-screen flex justify-center items-center'>
+            <div className="card shadow bg-white p-10">
+                <h1 className='text-center font-bold text-2xl border-b border-stone-300 pb-5'>Register your Account</h1>
+
+                <form action="" className='mt-5 space-y-5'>
+                    <label  className='font-bold ' >Your Name</label><br />
+                    <input type="text" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Name' name="name" id="" /><br />
+
+                
+                    <label className='font-bold ' >Photo URL</label>
+                    <input type="text" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Photo URL' name="photo" id="" />
+
+                    <label  className='font-bold ' >Email Address</label><br />
+                    <input type="email" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Email' name="email" id="" /><br />
+
+                    <label className='font-bold ' >Email Address</label>
+                    <input type="text" className='input bg-stone-100 mt-3 w-full' placeholder='Enter Your Password' name="password" id="" />
+
+                    <button type='submit' className='btn bg-[#403F3F] w-full text-white font-bold'>Register</button>
+
+                    
+                </form>
+               
+            </div>
         </div>
     );
 };
